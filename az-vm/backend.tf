7a43b712c7380b2e4f50adb0697bedbb0240c2ab
Jaya <30650274+jayachandrareddym@users.noreply.github.com>
2022-01-10 17:11:38 +0530

    Update backend.tf

--- az-vm/backend.tf
+++ az-vm/backend.tf
@@ -1,16 +1,1 @@
-terraform {
-  required_version = ">= 1.0.0"
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = ">= 2.0" # Optional but recommended in productions
-      features{}
-    }
-  }
- backend "azurerm" {
-    resource_group_name   = "AksTerraform-RG"
-    storage_account_name  = "jayaaksterraform"
-    container_name        = "tfstate"
-    key                   = "terraform-dev.tfstate"
-  }
-}
+
