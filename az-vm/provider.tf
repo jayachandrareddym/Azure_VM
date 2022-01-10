12d73af75c75ce1a8e0a2424c1b76679c4bea548
Jaya <30650274+jayachandrareddym@users.noreply.github.com>
2022-01-10 15:30:24 +0530

    Create provider.tf

---
+++ az-vm/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.0" # Optional but recommended in productions
+    }
+  }
+ backend "azurerm" {
+    resource_group_name   = "AksTerraform-RG"
+    storage_account_name  = "jayaaksterraform"
+    container_name        = "tfstate"
+    key                   = "terraform-dev.tfstate"
+  }
+}
