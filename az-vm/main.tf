1a9709b111642c9fe35509894e0fbb244e710180
Jayachandra Majjiga <Jayachandra_Majjiga@epam.com>
2022-01-10 13:18:50 +0530

    modifed provider and baxkend

--- az-vm/main.tf
+++ az-vm/main.tf
@@ -1,12 +1,17 @@
 terraform {
-  backend "azurerm" {
+  required_version = ">= 1.0.0"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.0" # Optional but recommended in productions
+    }
   }
-}
-
-provider "azurerm" {
-  version = ">=2.0"
-  # The "feature" block is required for AzureRM provider 2.x.
-  features {}
+ backend "azurerm" {
+    resource_group_name   = "AksTerraform-RG"
+    storage_account_name  = "jayaaksterraform"
+    container_name        = "tfstate"
+    key                   = "terraform-dev.tfstate"
+  }
 }
 
 variable "prefix" {
