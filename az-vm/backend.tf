0909242ae1281b18bb9ba90303a9030f60b729ea
Jaya <30650274+jayachandrareddym@users.noreply.github.com>
2022-01-10 17:40:10 +0530

    Update backend.tf

--- az-vm/backend.tf
+++ az-vm/backend.tf
@@ -1,1 +1,9 @@
-
+# Configure the Azure Provider
+terraform {
+ backend "azurerm" {
+    resource_group_name   = "AksTerraform-RG"
+    storage_account_name  = "jayaaksterraform"
+    container_name        = "tfstate"
+    key                   = "terraform-dev.tfstate"
+  }
+}
