27077ccdb6dfd180b6abc7c2fa5825ba9c99b954
Jaya <30650274+jayachandrareddym@users.noreply.github.com>
2022-01-11 14:24:20 +0530

    Update provider.tf

--- az-vm/provider.tf
+++ az-vm/provider.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "azurerm" {
     subscription_id = "d27e7e46-ee4b-4636-a893-80884653852d"
-    client_id = "36046737-0667-406b-be84-b5025b531df5"
+    client_id = "ca2129a7-26c7-4b22-b0ed-3852fef37e74"
     client_secret = "omY7Q~f3vw1HiY~1sQGNCviiuVW0CbbTOvyLp"
     tenant_id = "b41b72d0-4e9f-4c26-8a69-f949f367c91d"
     features {}
