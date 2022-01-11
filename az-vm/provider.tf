f0e36a0d08f321ba1ff9b0f13bb2a86800f59ce4
Jaya <30650274+jayachandrareddym@users.noreply.github.com>
2022-01-11 12:20:55 +0530

    Update provider.tf

--- az-vm/provider.tf
+++ az-vm/provider.tf
@@ -1,13 +1,12 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
+      source = "hashicorp/azurerm"
       version = "2.91.0"
     }
   }
 }
 
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
-  features {}
+  # Configuration options
 }
