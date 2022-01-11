59d6b53a8696487b3d8257a34e62e377e01f7f8f
Jaya <30650274+jayachandrareddym@users.noreply.github.com>
2022-01-11 14:42:47 +0530

    Update main.tf

--- az-vm/main.tf
+++ az-vm/main.tf
@@ -2,29 +2,29 @@ variable "prefix" {
   default = "tfvm"
 }
 
-resource "azurerm_resource_group" "main" {
+resource "azurerm_resource_group" "tfvm" {
   name     = "${var.prefix}-rg"
   location = "East US 2"
 }
 
-resource "azurerm_virtual_network" "main" {
+resource "azurerm_virtual_network" "tfvm" {
   name                = "${var.prefix}-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.tfvm.location
+  resource_group_name = azurerm_resource_group.tfvm.name
 }
 
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
+  resource_group_name  = azurerm_resource_group.tfvm.name
+  virtual_network_name = azurerm_virtual_network.tfvm.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_network_interface" "main" {
+resource "azurerm_network_interface" "tfvm" {
   name                = "${var.prefix}-nic"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.tfvm.location
+  resource_group_name = azurerm_resource_group.tfvm.name
 
   ip_configuration {
     name                          = "testconfiguration1"
@@ -33,11 +33,11 @@ resource "azurerm_network_interface" "main" {
   }
 }
 
-resource "azurerm_virtual_machine" "main" {
+resource "azurerm_virtual_machine" "tfvm" {
   name                  = "${var.prefix}-vm"
-  location              = azurerm_resource_group.main.location
-  resource_group_name   = azurerm_resource_group.main.name
-  network_interface_ids = [azurerm_network_interface.main.id]
+  location              = azurerm_resource_group.tfvm.location
+  resource_group_name   = azurerm_resource_group.tfvm.name
+  network_interface_ids = [azurerm_network_interface.tfvm.id]
   vm_size               = "Standard_DS1_v2"
 
   # comment this line to protect the OS disk and data automatically when deleting the VM
